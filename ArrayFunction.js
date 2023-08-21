0a5ce0186a4483dba2f79c0c001338f31fe8fd73
Mohammad Mansur Ali <80177222+mohammedmansurali92@users.noreply.github.com>
2023-08-21 12:30:59 -0700

    Array Function Info.
    This file has included array Function.

---
+++ ArrayFunction.js
@@ -0,0 +1,8 @@
+var names = new Array(5);
+names[0] = "Mansur";
+names[1] = "Tahmina";
+names[2] = "Towfiqur";
+names[3] = "Bokul";
+names[4] = "Mukul";
+document.write(names[2]);
+document.write(3);
